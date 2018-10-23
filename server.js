c39a6f8ab80f87bbf2e6f53a687917a6cc1a0865
Rubem Kalebe <rubemkalebe@gmail.com>
2018-10-22 23:24:27 -0300

    Starting using Node.js as server

--- server.js
+++ server.js
@@ -1,30 +1,58 @@
-var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var path = require('path')
 var fs = require('fs');
 
-http.createServer(function(req, res) {
-	if(req.method === "POST") {
-		var contentToSave;
-		req.on("data", function(data) {
-			contentToSave = data.toString().split('=')[1];
-			console.log(contentToSave);
-		});
+var app = express();
+app.set('port', 8080);
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-		req.on("end", function() {
-			res.writeHead(200, {
-				'Content-Type': 'application/json',
-				'Access-Control-Allow-Origin': '*'
-			});
-			//res.end('Post received');
-			fs.writeFile('tasks.json', contentToSave, function(err) {
-				if(err) throw err;
-				res.end(contentToSave);
-			});
-		});
-	} else if (req.method === "GET") {
-		res.writeHead(200, {
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*'
-		});
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
+
+app.route('/login')
+    .get((req, res) => {
+        res.sendFile(__dirname + '/public/login.html')
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+        console.log(username + ":" + password);
+        res.redirect('/home');
+    });
+
+app.route('/register')
+    .get((req, res) => {
+        res.sendFile(__dirname + '/public/register.html')
+    })
+    .post((req, res) => {
+        var nome = req.body.nome,
+        	usuario = req.body.usuario,
+        	senha = req.body.senha,
+            corfirmSenha = req.body.corfirmSenha;
+        console.log(nome + " " + usuario + " " + senha + " " + corfirmSenha);
+        res.redirect('/login');
+    });
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/public/home.html');
+});
+
+app.route('/tasks')
+    .get((req, res) => {
 		fs.createReadStream('tasks.json', 'UTF-8').pipe(res);
-	}
-}).listen(8080);
+    })
+    .post((req, res) => {
+        var contentToSave = req.body['tarefas'];
+        console.log(contentToSave);
+        fs.writeFile('tasks.json', contentToSave, function(err) {
+			if(err) throw err;
+			res.json(contentToSave);
+		});
+    });
+
+app.listen(app.get('port'), () => console.log(`App started on port ${app.get('port')}`));
